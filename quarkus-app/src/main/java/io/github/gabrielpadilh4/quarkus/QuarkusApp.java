d0bccced00e31bc6b2e71e8c6a016a7045735f8e
gabrielpadilh4 <gabrielpadilhasantos@gmail.com>
2025-07-28 21:13:35 -0300

    Adding Panache ORM

--- quarkus-app/src/main/java/io/github/gabrielpadilh4/quarkus/QuarkusApp.java
+++ quarkus-app/src/main/java/io/github/gabrielpadilh4/quarkus/QuarkusApp.java
@@ -21,9 +21,12 @@ public class QuarkusApp implements QuarkusApplication {
     @ActivateRequestContext
     public int run(String... args) throws Exception {
 
-        List<Service> services = this.serviceRepository.getAllServices();
+        List<Service> services = this.serviceRepository.listAll();
 
         services.forEach(System.out::println);
+
+        Service service = this.serviceRepository.findById(2L);
+        System.out.println(service);
 
         return 0;
     }
